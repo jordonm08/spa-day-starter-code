86af0c0d8620d7c242089702e01f957a0213eade
jordonm08 <jorodnm08@gmail.com>
2021-06-04 12:22:13 -0500

    update

---
+++ src/main/java/org/launchcode/spaday/controllers/UserController.java
@@ -0,0 +1,34 @@
+package org.launchcode.spaday.controllers;
+
+import org.launchcode.spaday.models.User;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+
+@RequestMapping("users")
+public class UserController {
+
+    @GetMapping("add")
+    public String displayAddUserForm() {
+        return "users/add";
+    }
+
+    @PostMapping
+    public String processAddUserForm(Model model, @ModelAttribute User user, String verify) {
+        model.addAttribute("user", user);
+        model.addAttribute("verify", verify);
+        model.addAttribute("username", user.getUsername());
+        model.addAttribute("email", user.getEmail());
+
+        if (user.getPassword().equals(verify)){
+            return "users/index";
+        } else {
+            return "users/add";
+        }
+    }
+}
